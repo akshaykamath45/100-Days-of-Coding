aa8e5e01e10a066e8c46e2fa6f7b9168b9121557
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-01-28 04:22:00 +0530

    Day 5
    Write a program to identify if the number is even or odd.
    
    Get a number as input from the user and check whether the given number is odd or even.
    
    Input:10
    Output:Even
    
    Input:5
    Output:Odd

---
+++ D5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int n;
+   cin>>n;
+   if(n%2==0){
+       cout<<"Even"<<endl;//Considering 0 as an even number as well.
+   }else{
+       cout<<"Odd"<<endl;
+   }
+}
