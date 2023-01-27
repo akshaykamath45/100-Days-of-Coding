f1f3e739440e375d5bd90f597dcb2c898f746550
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-01-28 03:57:43 +0530

    Day 1
    Write a program to identify if the character is a vowel or consonant.
    
    Take an input character from the user and check whether the given input is a vowel or consonant.
    
    Input
    A
    
    Output
    Vowel
    
    Input
    m
    
    Output
    Consonant
    
    Input
    3
    
    Output
    Invalid Input

--- D1.cpp
+++ D1.cpp
@@ -8,8 +8,11 @@ int main()
     if(c=='A' || c=='E' || c=='I' || c=='O' || c=='U' || c=='a' || c=='e' || c=='i' || c=='o' || c=='u'){
         cout<<"Vowel"<<endl;
     }
-    else{
+    else if(c>='A'&&c<='Z' || c>='a' && c<='z'){
         cout<<"Consonant"<<endl;
+    }
+    else{
+        cout<<"Invalid Input"<<endl;
     }
 
     return 0;
